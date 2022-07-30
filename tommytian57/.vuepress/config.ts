033d51878cbda3b778ccada4cb3a47d216a9d194
tommytian57 <daisdroner@gmail.com>
2022-07-29 22:57:10 -0400

    update

--- tommytian57/.vuepress/config.ts
+++ tommytian57/.vuepress/config.ts
@@ -25,7 +25,7 @@ export default defineUserConfig({
       // update: "force",
       update: "hint",
       // maxSize: 6,
-      favicon: "/TTREK.jpg",
+      favicon: "/ttrek.jpg",
     }),
     
     searchPlugin({
